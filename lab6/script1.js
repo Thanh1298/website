36954a231257be4b69088a9c4d77a2f269ec1c20
Nguyễn Văn Thanh <23130304@st.hcmuaf.edu.vn>
2024-12-18 18:14:15 +0700

    Delete lab6 directory

--- lab6/script1.js
+++
@@ -1,7 +0,0 @@
-// This can be used for adding interactive features if needed
-document.querySelectorAll('.category').forEach(button => {
-    button.addEventListener('click', () => {
-        document.querySelector('.category.active').classList.remove('active');
-        button.classList.add('active');
-    });
-});
